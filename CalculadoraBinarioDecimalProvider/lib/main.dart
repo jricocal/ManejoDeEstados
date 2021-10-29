aeaca339659ebd81ff7a2f8d58b63997e91d4dfd
kristellu <kristellu@uninorte.edu.co>
2021-10-29 08:30:49 -0500

    added: controllers file

--- CalculadoraBinarioDecimalProvider/lib/main.dart
+++ CalculadoraBinarioDecimalProvider/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:demo_app/ui/app.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'domain/provider/convertion_model.dart';
+import 'domain/models/provider/convertion_model.dart';
 
 void main() => runApp(
       ChangeNotifierProvider(
--- CalculadoraBinarioDecimalProvider/lib/ui/widgets/dec2bin.dart
+++ CalculadoraBinarioDecimalProvider/lib/ui/widgets/dec2bin.dart
@@ -1,4 +1,4 @@
-import 'package:demo_app/domain/provider/convertion_model.dart';
+import 'package:demo_app/domain/models/provider/convertion_model.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
